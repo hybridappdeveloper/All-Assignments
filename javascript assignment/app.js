271603b81b20bc93c66aca5b57e689f6ca68aeca
hybridappdeveloper <sanjayhansrajani@gmail.com>
2020-06-14 16:33:00 +0500

    Javascript practice assignment

---
+++ javascript assignment/app.js
@@ -0,0 +1,127 @@
+// assignment 
+// chapter-1 alert
+// alert("Welcome to my website")
+
+// alert("Error! Please enter a valid password.")
+
+// var a="Welcome to JS Land...";
+// var b="Happy Coding!";
+// var c= a+ "<br>" + b;
+// alert(c);
+
+// alert("Welcome to JS Land...")
+// var a="Happy Coding!"
+// var b="prevent this page from creating additional dialogs."
+// alert(a+b);
+
+// var a= "Hello... I can run JS through my web browser's console"
+// console.log(a);
+
+// chapter-2
+// var myName="Jhone Doe"
+// var username="15 years old"
+// var message="Hello World"
+// alert(myName);
+// alert(username);
+// alert(message);
+
+// var my="My email address is "
+// var email="example@example.com"
+// alert(my+email)
+
+// var learn="I am trying to learn from the Book "
+// var book="A smarter way to learn JavaScript."
+// alert(learn+book)
+
+// document.write("Yah! I can write HTML content through JavaScript")
+
+// var i= "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(i)
+
+// chapter-3
+// var age="I am 15 years old"
+// alert(age);
+
+// var visit="You have visited this site 14 times"
+// alert(visit);
+
+// var birthyear="My birth year is 1990"
+// document.write(birthyear)
+
+// var name=prompt("Enter your name ")
+// var product=prompt(" Enter your product ")
+// var quantity=prompt(" Enter your quantity ")
+// document.write(name + " ordered " + quantity +" " + product  + " on XYZ Clothing Store.")
+
+// chapter-4
+// var a= "Rules for naming JS variables";
+// var b= "Variable names can only contain, numbers,$ and _.For example $my_1stVariable."
+// var c= "Variables must begin with a letter, $ or_____. Forexample $name, _name or name"
+// var d= "Variable names are case sensetive "
+// var e= "Variable names should not be JS Keywords"
+// document.write("<b>" +a + "</b>" + "<br>"+ "<br>" + "<br>" + b+ "<br>" +c+"<br>" +d+"<br>" +e)
+
+// chapter-5
+// var num1=prompt("Enter your number")
+// var num2=prompt("Enter your number")
+// var add=(+num1) + (+num2)
+// document.write("Sum of "+ num1 +" "+ "and"+" "+ num2+ " is" +" "+ add)
+
+// var num1=prompt("Enter your number")
+// var num2=prompt("Enter your number")
+// var sub=(num1) - (num2)
+// document.write("Subtraction of "+ num1 +" "+ "and"+" "+ num2+ " is" +" "+ sub)
+
+// var num1=prompt("Enter your number")
+// var num2=prompt("Enter your number")
+// var mul=(num1) * (num2)
+// document.write("Multiplication of "+ num1 +" "+ "and"+" "+ num2+ " is" +" "+ mul)
+
+// var num1=prompt("Enter your number")
+// var num2=prompt("Enter your number")
+// var div=(num1) / (num2)
+// document.write("Division of "+ num1 +" "+ "and"+" "+ num2+ " is" +" "+ div)
+
+// var num1=prompt("Enter your number")
+// var num2=prompt("Enter your number")
+// var mod=(num1) % (num2)
+// document.write("Modulus of "+ num1 +" "+ "and"+" "+ num2+ " is" +" "+ mod)
+
+// var ticket=prompt("Enter number of tickets you want to buy");
+// var price="600"
+// var total=(ticket*price)
+// document.write("Total cost to buy" +" "+ ticket +" "+ "ticketes to a movie is"+" "+ total+ " "+"PKR")
+
+// for(var a=1; a <=10; a++){
+//     document.write("4" + "X" + a + "=" + 4*a + "<br>")
+// }
+
+
+// var item1=prompt("Enter your item")
+// var qty1=prompt("Enter your quantity")
+// var price1 ="650"
+// var item2=prompt("Enter your item")
+// var qty2=prompt("Enter your quantity")
+// var price2 ="100"
+// var shippingcharges = "100"
+// var total=((qty1*price1)+ (qty2*price2)+(+shippingcharges))
+// document.write("Total cost of your order is" +" "+ total )
+
+
+// document.write
+// ("Total Cost of item 1 is " +item1*qtyitem1 ("<br>"),
+// "Total Cost of item 2 is " +item2*qtyitem2)
+
+// var marks=prompt("Enter your total marks")
+// var obt=prompt("Enter your obtained marks")
+// var per=(obt/marks*100)
+// document.write("Total marks:" +marks +"<br>"+ "Marks Obtained:"+obt+ "<br>"+ "Percentage:" +per+"%")
+
+// var fav=prompt("Enter Your Favourite Snack ")
+// var age=prompt("Enter Your Current Age")
+// var max=prompt("Enter Your Estimated Maximum Age")
+// var snacks=prompt("Enter snacks per day")
+// var total=((max-age)*(snacks));
+// document.write("You will need"+" "+ total+" "+ fav+" "+ "to last you until the ripe old age of"+" " +max  )
+
+
